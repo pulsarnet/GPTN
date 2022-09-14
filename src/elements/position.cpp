5d771cbd89a54c900a0e11b1b32fff824c3252da
NICK <darkpaladin2000@gmail.com>
2022-09-14 13:23:35 +0300

    Better draw

--- src/elements/position.cpp
+++ src/elements/position.cpp
@@ -1,29 +1,71 @@
 #include "position.h"
 #include "transition.h"
-#include "../ffi/rust.h"
+#include <QFontMetrics>
 
 Position::Position(const QPointF& origin, ffi::PetriNet* net, ffi::VertexIndex position, QGraphicsItem* parent) : PetriObject(net, position, parent) {
     this->setPos(origin);
 }
 
 QRectF Position::boundingRect() const {
-    return shape().boundingRect().adjusted(-2, -2, 2, 2);
+    return shape().boundingRect().adjusted(-2, -20, 2, 20);
 }
 
 void Position::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) {
 
     qreal penWidth = 2;
 
-    painter->setClipRect(boundingRect());
-
     painter->save();
     painter->setPen(QPen(isSelected() ? Qt::green : painter->pen().color(), penWidth));
     painter->drawEllipse(boundingRect().center(), radius - penWidth / 2., radius - penWidth / 2.);
 
-    auto name = QString("%1%2").arg(this->index()).arg(this->markers() == 0 ? "" : QString("(%1)").arg(
-                                                                this->markers()));
+    auto name = QString("P%1%2")
+            .arg(this->index())
+            .arg(this->vertex()->parent() == 0 ? "" : QString(".%1").arg(this->vertex()->parent()));
 
-    painter->drawText(boundingRect(), Qt::AlignCenter, name);
+    QFontMetricsF metric = QFontMetricsF(painter->font());
+    QSizeF nameSize = metric.size(0, name);
+    painter->drawText(boundingRect().center().x() - nameSize.width() / 2
+                      , boundingRect().center().y() - radius - 5
+                      , name);
+
+    if (this->markers() == 1) {
+        painter->save();
+        painter->setBrush(QBrush(Qt::black));
+        painter->drawEllipse(boundingRect().center(), 3., 3.);
+        painter->restore();
+    } else if (this->markers() == 2) {
+        auto rect = boundingRect();
+        painter->save();
+        painter->setBrush(QBrush(Qt::black));
+
+        rect.setX(rect.x() - 9.);
+        painter->drawEllipse(rect.center(), 3., 3.);
+
+        rect.setX(rect.x() + 18.);
+        painter->drawEllipse(rect.center(), 3., 3.);
+
+        painter->restore();
+    } else if (this->markers() == 3) {
+        auto rect = boundingRect();
+        painter->save();
+        painter->setBrush(QBrush(Qt::black));
+
+        rect.setX(rect.x() - 9.);
+        rect.setY(rect.y() + 9.);
+        painter->drawEllipse(rect.center(), 3., 3.);
+
+        rect.setX(rect.x() + 18.);
+        painter->drawEllipse(rect.center(), 3., 3.);
+
+        rect.setX(rect.x() - 9.);
+        rect.setY(rect.y() - 18.);
+        painter->drawEllipse(rect.center(), 3., 3.);
+
+        painter->restore();
+    } else if (this->markers() > 3) {
+        painter->drawText(boundingRect(), Qt::AlignCenter, QString("%1").arg(this->markers()));
+    }
+
     painter->restore();
 }
 
